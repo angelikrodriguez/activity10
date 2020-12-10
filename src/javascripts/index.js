a4c1beba1de29b5faacaf779f1831fc65295058d
angelikrodriguez <mrodriguezperalta@mail.weber.edu>
2020-12-09 18:30:32 -0700

    Activity10

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -4,6 +4,44 @@ require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
-//TODO
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {movies} from './movies'
+
+
+let featured_movie = document.querySelector('.featured')
+for(let m of movies){
+    let movie_thumb = document.getElementById('m' + m.id)
+    movie_thumb.innerHTML = `
+    <img src="${m.poster}">
+    `
+    movie_thumb.onclick = function(){
+        selectMovie(m)
+    }
+}
+
+
+function selectMovie(m){
+    featured_movie.innerHTML= `
+    <img src="${m.poster}" style="float : left;" >
+    <h1>${m.title}</h1>
+    <p>${m.plot}<p>
+    `
+}
+function searchMovies(event){
+    if(event){
+        event.preventDefault()
+    }
+    let input = document.querySelector('[type="search"]').value || ""
+    for(let m of movies){
+       let movie_thumb = document.getElementById('m' + m.id)
+        if(m.title.toUpperCase().indexOf(input.toUpperCase())==-1){
+            movies_thumb.classList.add('hidden')
+        }else{
+             movies_thumb.classList.remove('hidden')
+        }
+    }
+    }
+document.querySelector('button').onclick = searchMovies
+document.querySelector('[type="search"]').onsearch = searchMovies
+document.form[0].addEventListener('submit',searchMovies,false)
